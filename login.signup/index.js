47a58d8598c66a548de88779bf78f7c794b0745b
Rijul Mandloi <mandloi.rijul@gmail.com>
2023-04-24 23:04:06 +0530

    c

--- login.signup/index.js
+++ login.signup/index.js
@@ -81,43 +81,12 @@ function isValidEmail(email) {
   return emailRegex.test(email);
 }
 
+const user = auth.currentUser;
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+  } 
+  else {
 
-// login.addEventListener('click',(e)=>{
-//   var email = document.getElementById('email').value;
-//   var password = document.getElementById('password').value;
-
-//      signInWithEmailAndPassword(auth, email, password)
-//      .then((userCredential) => {
-//        // Signed in 
-//        const user = userCredential.user;
-
-//        const dt = new Date();
-//         update(ref(database, 'users/' + user.uid),{
-//          last_login: dt,
-//        })
-
-//        window.location.href = '../home.page/index.html';
-//        // ...
-//      })
-//      .catch((error) => {
-//        const errorCode = error.code;
-//        const errorMessage = error.message;
-
-//        alert(errorMessage);
-//  });
-// });
-
-// const user = auth.currentUser;
-// onAuthStateChanged(auth, (user) => {
-//   if (user) {
-//     // User is signed in, see docs for a list of available properties
-//     // https://firebase.google.com/docs/reference/js/firebase.User
-//     const uid = user.uid;
-//     //bla bla bla
-//     // ...
-//   } else {
-//     // User is signed out
-//     // ...
-//     //bla bla bla
-//   }
-// });
+  }
+});
